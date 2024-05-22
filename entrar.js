b24c42d120228031b521469d7cd3b00aa65416fe
Augusto Martinez <augusto.mmartinez@gmail.com>
2024-05-22 20:40:34 -0300

    Entrar completo

---
+++ entrar.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function(){
+    document.querySelector('.form-error').value = '';
+    const form = document.querySelector('form');
+
+    form.addEventListener('submit', function(e){
+        e.preventDefault();
+
+        const usuario = document.getElementById('usuario').value;
+        const senha = document.getElementById('senha').value;
+
+        fetch(`https://664e79e7fafad45dfae02a8d.mockapi.io/linguagem/ac2_9?email=${usuario}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            const user = data.find(u => u.senha === senha);
+            if(usuario){
+                console.log("Usuário Encontrado: ", user);
+            } 
+            else{
+                document.getElementById('nome-error').textContent = "Senha Incorreta!";
+            }
+        })
+        .catch((error) => {
+            console.error('Erro na Requisição: ', error);
+            document.getElementById('nome-error').textContent = 'Usuário não Encontrado';
+        });
+    });
+});
