6cc7861a175828356a19056f5a4ca6e43ffda78c
Augusto Martinez <augusto.mmartinez@gmail.com>
2024-05-22 22:21:44 -0300

    a

--- parabens.js
+++ parabens.js
@@ -1,4 +1,15 @@
 document.addEventListener('DOMContentLoaded', function(){
     var usuario = sessionStorage.getItem('Usuario');
-    document.getElementById('nomeUser').textContent = 'Seja muito bem vindo ' + usuario + '!';
+    if(usuario){
+        document.getElementById('nomeUser').textContent = 'Seja muito bem vindo ' + usuario + '!';
+    }
+
+    var formulario = document.getElementById('form');
+    
+    if (formulario) {
+        formulario.addEventListener('submit', function(event) {
+            event.preventDefault();
+            window.location.href = 'entrar.html';
+        });
+    }
 });
